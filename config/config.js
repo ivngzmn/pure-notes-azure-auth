78b390ae22bda7731654aa3b520d5189f6edfcf5
Ivan Guzman <n3cr0.2012@gmail.com>
2021-08-09 00:24:44 -0700

    initial commit

---
+++ config/config.js
@@ -0,0 +1,49 @@
+require("dotenv").config({ path: "./config/.env" });
+
+exports.creds = {
+  identityMetadata:
+    "https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration",
+
+  clientID: process.env.CLIENT_ID,
+
+  clientSecret: process.env.CLIENT_SECRET,
+
+  responseType: "code id_token",
+
+  responseMode: "form_post",
+
+  redirectUrl: "http://localhost:5000/auth/openid/return",
+
+  allowHttpForRedirectUrl: true,
+
+  validateIssuer: false,
+
+  issuer: null,
+
+  passReqToCallback: false,
+
+  useCookieInsteadOfSession: false,
+
+  cookieEncryptionKeys: [
+    { key: "12345678901234567890123456789012", iv: "123456789012" },
+    { key: "abcdefghijklmnopqrstuvwxyzabcdef", iv: "abcdefghijkl" },
+  ],
+
+  scope: ["profile", "offline_access", "https://graph.microsoft.com/mail.read"],
+
+  loggingLevel: false,
+
+  nonceLifetime: null,
+
+  nonceMaxAmount: 5,
+
+  clockSkew: null,
+};
+
+exports.destroySessionUrl = "http://localhost:5000";
+
+exports.useMongoDBSessionStore = false;
+
+exports.databaseUri = "mongodb://localhost/OIDCStrategy";
+
+exports.mongoDBSessionMaxAge = 24 * 60 * 60;
